c81626ec61a09b06e50772225198f34b6b56964c
Léo ROCHARD <leo.rochard@viacesi.fr>
2023-02-03 15:13:21 +0100

    feat: Ajout du SaveProject

---
+++ ProjetMVC/Model/Progression.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetMVC.Model
+{
+    class Progression
+    {
+        // Remaining file amount
+        private int remainingFileAmount;
+        public int RemainingFileAmount
+
+        {
+            get { return remainingFileAmount; }
+            set { remainingFileAmount = value; }
+        }
+        // Remaining file size 
+        private int remainingFileSize;
+        public int RemainingFileSize
+        {
+            get { return remainingFileSize; }
+            set { remainingFileSize = value; }
+        }
+
+        public Progression(int remainingFileAmount, int remainingFileSize)
+        {
+            this.remainingFileAmount = remainingFileAmount;
+            this.remainingFileSize = remainingFileSize;
+        }
+    }
+}
---
+++ ProjetMVC/Model/SaveProject.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetMVC.Model
+{
+    class SaveProject
+    {
+        // Project Name
+        private string name;
+        private string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        // Project repertory source
+        private string repertorySource;
+        private string RepertorySource
+        {
+            get { return repertorySource; }
+            set { repertorySource = value; }
+        }
+
+        // Project repertory target
+        private string repertoryTarget;
+        private string RepertoryTarget
+        {
+            get { return repertoryTarget; }
+            set { repertoryTarget = value; }
+        }
+        // Project progression
+        private Progression progression;
+        private Progression Progression
+        {
+            get { return progression; }
+            set { progression = value; }
+        }
+        // Project start time
+        private DateTime startTime;
+        private DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        // File size to save
+        private int fileSize;
+        private int FileSize
+        {
+            get { return fileSize; }
+            set { fileSize = value; }
+        }
+
+        // Save project type
+        private SaveTypeEnum saveType;
+        public SaveTypeEnum SaveType
+        {
+            get { return saveType; }
+            set { saveType = value; }
+        }
+
+        public SaveProject(string name, string repertorySource, string repertoryTarget, SaveTypeEnum saveType)
+        {
+            this.name = name;
+            this.repertorySource = repertorySource;
+            this.repertoryTarget = repertoryTarget;
+            this.saveType = saveType;
+
+        }
+
+        // TODO: Méthode pour démarrer le processus de sauvegarde, définir : fileSize et la progression 
+        public void save() { }
+        public SaveProject getInfo()
+        {
+            return this;
+        }
+
+    }
+}
