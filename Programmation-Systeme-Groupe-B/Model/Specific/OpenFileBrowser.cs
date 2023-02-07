a649c5ee842da0ec025e368e677141c4f5d2bab0
Frogounet <thefrogman59@gmail.com>
2023-02-07 15:32:04 +0100

    début MVVM

---
+++ Programmation-Systeme-Groupe-B/Model/Specific/OpenFileBrowser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Programmation_Systeme_Groupe_B.ViewModels;
+
+namespace Programmation_Systeme_Groupe_B.Model.Specific
+{
+    class OpenFileBrowser : ICommand
+    {
+        private ViewModel viewModel;
+        
+        public OpenFileBrowser(ViewModel vm)
+        {
+            viewModel = vm;
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.OpenFileBrowserCommand();
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ Programmation-Systeme-Groupe-B/ViewModels/ViewModelBase1.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+namespace Programmation_Systeme_Groupe_B.ViewModels
+{
+    class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
