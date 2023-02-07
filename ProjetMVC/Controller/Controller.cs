bb2c12eed24de9256736b9400bb60f8a17808b39
Frogounet <thefrogman59@gmail.com>
2023-02-07 10:35:19 +0100

    case 1 done

--- ProjetMVC/Controller/Controller.cs
+++ ProjetMVC/Controller/Controller.cs
@@ -30,7 +30,6 @@ namespace ProjetMVC.Controller
                 switch (option)
                 {
                     case "1":
-                        // "1 : Create a new save project
                         if (!modelClass.CheckNumProject())
                         {
                             viewClass.WriteLine(modelClass.modelLangage.ErrorTooManyProject());
@@ -58,20 +57,17 @@ namespace ProjetMVC.Controller
                                 break;
                         }
                         SaveProject saveproject = new SaveProject(name,pathS,pathT,saveType );
+                        modelClass.ModelSave.addProject(saveproject);
                         // "1 : Create a new save project
                         break;
                     case "2":
                         // "2 : Modify an existing project
-                        viewClass.WriteLine(modelClass.modelLangage.NotImplementedMsg());
                         break;
                     case "3":
                         // "3 : List all existing projects
                         break;
                     case "4":
                         // "4 : Start a save project
-                        viewClass.WriteLine(modelClass.modelLangage.AskWhichSave());
-                        int savenum = Convert.ToInt32(viewClass.ReadLine());
-                        modelClass.ModelSave.Projects[savenum].Save();
                         break;
                     case "5":
                         // "5 : Start all save projects
