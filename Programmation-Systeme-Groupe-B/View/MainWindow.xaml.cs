40b58c758a59d112e5c543a5de15e9ede0faa153
Frogounet <thefrogman59@gmail.com>
2023-02-10 10:37:47 +0100

    y'a r

--- Programmation-Systeme-Groupe-B/View/MainWindow.xaml.cs
+++ Programmation-Systeme-Groupe-B/View/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Programmation_Systeme_Groupe_B.View
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : UserControl
+    public partial class MainWindow : Window
     {
         public MainWindow()
         {
