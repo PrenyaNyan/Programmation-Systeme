38ff5ce7494ec903c90ecb62db7d03d70d58142c
Asarck <85520089+Asarck@users.noreply.github.com>
2023-02-16 13:07:09 +0100

    "NE PAS PRENDRE CE COMMIT"

---
+++ Programmation-Systeme-Groupe-B/Model/Specific/GetInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Programmation_Systeme_Groupe_B.ViewModels;
+
+namespace Programmation_Systeme_Groupe_B.Model.Specific
+{
+    class GetInfo : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private ViewModel viewModel;
+        public GetInfo(ViewModel vm)
+        {
+            viewModel = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.GetInfoCommand();
+        }
+    }
+}
