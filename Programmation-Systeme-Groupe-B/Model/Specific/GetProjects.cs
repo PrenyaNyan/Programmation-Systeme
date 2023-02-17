0a38bb6166a84e5e5af6c29f42aedd9505e4d193
Alexandre <alexandrepereon8@gmail.com>
2023-02-17 11:24:00 +0100

    Rebluid la connerie

---
+++ Programmation-Systeme-Groupe-B/Model/Specific/GetProjects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Programmation_Systeme_Groupe_B.ViewModels;
+using System.Windows.Input;
+
+namespace Programmation_Systeme_Groupe_B.Model.Specific
+{
+    class GetProjects : ICommand
+    {
+        private ViewModel viewModel;
+
+        public GetProjects(ViewModel vm)
+        {
+            viewModel = vm;
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            viewModel.GetProjectsCommand();
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
