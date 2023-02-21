0d1c630a6276130607a77c9d35c37d3a8098a476
Léo ROCHARD <leo.rochard@viacesi.fr>
2023-02-21 09:42:58 +0100

    feat: mono-instance

---
+++ Programmation-Systeme-Groupe-B/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Programmation_Systeme_Groupe_B
+{
+    class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            bool prevInstance;
+            _ = new System.Threading.Mutex(true, "Easy Save", out prevInstance);
+            if (prevInstance == false)
+            {
+                MessageBox.Show("There is another instance running");
+                return;
+            }
+            App application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
+    }
+}
