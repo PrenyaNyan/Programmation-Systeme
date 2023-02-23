778c8414ed1c2d4afd790de0498b325241a2392b
Asarck <85520089+Asarck@users.noreply.github.com>
2023-02-23 11:30:40 +0100

    class diagram

--- Programmation-Systeme-Groupe-B/Model/Specific/CreateProjectBase.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Programmation_Systeme_Groupe_B.Model.Specific
-{
-    internal class CreateProjectBase
-    {
-        public event EventHandler CanExecuteChanged;
-    }
-}
